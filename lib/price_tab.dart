1c74fd2742001519bf5991343fe2ba0d5742a4d5
MarcinusX <marcin.szalek1@gmail.com>
2018-07-22 13:42:26 +0200

    Added dots animation

--- lib/price_tab.dart
+++ lib/price_tab.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class PriceTab extends StatefulWidget {
@@ -11,13 +13,41 @@ class PriceTab extends StatefulWidget {
   _PriceTabState createState() => _PriceTabState();
 }
 
+class FlightStop {
+  String from;
+  String to;
+  String date;
+  String duration;
+  String price;
+  String fromToTime;
+
+  FlightStop(this.from, this.to, this.date, this.duration, this.price,
+      this.fromToTime);
+}
+
 class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
   AnimationController _initialPlainAnimationController;
-  Animation<double> _plainSizeAnimation;
+  Animation<double> _planeSizeAnimation;
   AnimationController _planeTravelAnimationController;
   Animation<double> _planeTravelAnimation;
+  AnimationController _dotsAnimationController;
   final double _startPaddingBottom = 16.0;
   final double _endPaddingTop = 16.0;
+  final double _flightStopHeight = 80.0;
+  final List<FlightStop> _flightStops = [
+    FlightStop("JFK", "ORY", "JUN 05", "6h 25m", "\$851", "9:26 am - 3:43 pm"),
+    FlightStop("MRG", "FTB", "JUN 20", "6h 25m", "\$532", "9:26 am - 3:43 pm"),
+    FlightStop("ERT", "TVS", "JUN 20", "6h 25m", "\$718", "9:26 am - 3:43 pm"),
+    FlightStop("KKR", "RTY", "JUN 20", "6h 25m", "\$663", "9:26 am - 3:43 pm"),
+  ];
+  final List<Animation<double>> stopPositions = [];
+
+  double get _planeTopPadding =>
+      _endPaddingTop +
+          (1 - _planeTravelAnimation.value) *
+              (widget.height - _endPaddingTop - _startPaddingBottom - 40.0);
+
+  double get _planeIconSize => _planeSizeAnimation.value;
 
   @override
   void initState() {
@@ -34,19 +64,67 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        _initialPlainAnimationController.isCompleted
-            ? _buildTravelPlaneAnimation()
-            : _buildInitialPlaneAnimation(),
-      ],
+    return Container(
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          _initialPlainAnimationController.isCompleted
+              ? _buildTravelPlaneAnimation()
+              : _buildInitialPlaneAnimation(),
+        ]
+          ..addAll(
+            _flightStops.map(
+                  (stop) => _buildDot(stop),
+            ),
+          ),
+      ),
+    );
+  }
+
+  Widget _buildTravelPlaneAnimation() {
+    return AnimatedBuilder(
+      animation: _planeTravelAnimation,
+      builder: (context, child) {
+        return Positioned(
+          top: _planeTopPadding,
+          child: Column(
+            children: <Widget>[
+              _buildPlaneIcon(),
+              Container(
+                width: 2.0,
+                height: _flightStops.length * _flightStopHeight,
+                color: Color.fromARGB(255, 200, 200, 200),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildPlaneIcon() {
+    return Icon(
+      Icons.airplanemode_active,
+      color: Colors.red,
+      size: _planeIconSize,
+    );
+  }
+
+  Widget _buildInitialPlaneAnimation() {
+    return Positioned(
+      bottom: _startPaddingBottom,
+      child: AnimatedBuilder(
+        animation: _planeSizeAnimation,
+        builder: (context, child) => _buildPlaneIcon(),
+      ),
     );
   }
 
   void _initAnimations() {
     _initInitialAnimation();
     _initPlaneTravelAnimation();
+    _initDotAnimation();
   }
 
   void _initPlaneTravelAnimation() {
@@ -69,45 +147,52 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
         if (status == AnimationStatus.completed) {
           widget.onPlaneSizeAnimated();
           _planeTravelAnimationController.forward();
+          new Future.delayed(Duration(milliseconds: 200),
+                  () => _dotsAnimationController.forward());
         }
       });
-    _plainSizeAnimation = Tween<double>(begin: 60.0, end: 36.0)
+    _planeSizeAnimation = Tween<double>(begin: 60.0, end: 36.0)
         .animate(_initialPlainAnimationController);
   }
 
-  double get _planeTopPadding =>
-      _endPaddingTop +
-          (1 - _planeTravelAnimation.value) *
-              (widget.height - _endPaddingTop - _startPaddingBottom - 40.0);
-
-  Widget _buildTravelPlaneAnimation() {
-//    return Container();
+  Widget _buildDot(FlightStop stop) {
+    int index = _flightStops.indexOf(stop);
+    Color color = index.isEven ? Colors.red : Colors.green;
+    Animation<double> animation = stopPositions[index];
     return AnimatedBuilder(
-      animation: _planeTravelAnimation,
-      builder: (context, child) {
-        return Positioned(
-          top: _planeTopPadding,
-          child: _buildPlaneIcon(),
-        );
-      },
+      animation: animation,
+      builder: (context, child) =>
+          Positioned(
+            top: animation.value,
+            child: Container(
+              height: 16.0,
+              width: 16.0,
+              decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+            ),
+          ),
     );
   }
 
-  Widget _buildPlaneIcon() {
-    return Icon(
-      Icons.airplanemode_active,
-      color: Colors.red,
-      size: _plainSizeAnimation.value,
-    );
-  }
+  _initDotAnimation() {
+    _dotsAnimationController = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+    final delayInterval = 0.2;
+    final slideInterval = 0.4;
+    double startingPaddingTop = widget.height + 16.0;
+    double constMarginTop = 16.0 + _planeIconSize + 0.5 * _flightStopHeight;
+    for (int i = 0; i < _flightStops.length; i++) {
+      final start = delayInterval * i;
+      final end = start + slideInterval;
 
-  Widget _buildInitialPlaneAnimation() {
-    return Positioned(
-      bottom: _startPaddingBottom,
-      child: AnimatedBuilder(
-        animation: _plainSizeAnimation,
-        builder: (context, child) => _buildPlaneIcon(),
-      ),
-    );
+      double endingPaddingTop = constMarginTop + i * _flightStopHeight;
+      Animation<double> animation =
+      new Tween(begin: startingPaddingTop, end: endingPaddingTop).animate(
+        new CurvedAnimation(
+          parent: _dotsAnimationController,
+          curve: new Interval(start, end, curve: Curves.easeOut),
+        ),
+      );
+      stopPositions.add(animation);
+    }
   }
 }
