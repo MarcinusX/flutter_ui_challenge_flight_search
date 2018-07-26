3c99a1de73b068fd1523cb113380568d83bcab49
MarcinusX <marcin.szalek1@gmail.com>
2018-07-26 21:37:28 +0200

    Cleanup

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,68 +1,10 @@
-import 'package:flight_search/details_card.dart';
-import 'package:flight_search/rounded_button.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new Stack(
-        children: <Widget>[
-          new Container(
-            decoration: new BoxDecoration(
-              gradient: new LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Colors.red, const Color(0xFFE64C85)],
-              ),
-            ),
-            height: 200.0,
-          ),
-          new AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-            centerTitle: true,
-            title: new Text("AirAsia"),
-          ),
-          Positioned.fill(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: new Column(
-                children: <Widget>[
-                  _buttonsRow(),
-                  Expanded(child: DetailsCard()),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _buttonsRow() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          new RoundedButton(
-            text: "ONE WAY",
-          ),
-          new RoundedButton(
-            text: "ROUND",
-          ),
-          new RoundedButton(
-            selected: true,
-            text: "MULTICITY",
-          ),
-        ],
-      ),
+      body: Center(child: Text("Let's get started!")),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flight Search',
       theme: new ThemeData(
         primarySwatch: Colors.red,
       ),
