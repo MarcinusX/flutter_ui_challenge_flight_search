2361ae35ed6e8eaf179a24871bf04c8dfe2e6c5a
MarcinusX <marcin.szalek1@gmail.com>
2018-07-29 12:08:39 +0200

    Added dots animation

---
+++ lib/animated_dot.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AnimatedDot extends AnimatedWidget {
+  final Color color;
+
+  AnimatedDot({
+    Key key,
+    Animation<double> animation,
+    @required this.color,
+  }) : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    Animation<double> animation = super.listenable;
+    return Positioned(
+      top: animation.value,
+      child: Container(
+          height: 24.0,
+          width: 24.0,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+              border: Border.all(color: Color(0xFFDDDDDD), width: 1.0)),
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: DecoratedBox(
+              decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+            ),
+          )),
+    );
+  }
+}
--- lib/price_tab.dart
+++ lib/price_tab.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flight_search/animated_dot.dart';
 import 'package:flight_search/animated_plane_icon.dart';
 import 'package:flutter/material.dart';
 
@@ -17,18 +18,26 @@ class PriceTab extends StatefulWidget {
 class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
   final double _initialPlanePaddingBottom = 16.0;
   final double _minPlanePaddingTop = 16.0;
+  final List<int> _flightStops = [1, 2, 3, 4];
+  final double _cardHeight = 80.0;
 
   AnimationController _planeSizeAnimationController;
   AnimationController _planeTravelController;
+  AnimationController _dotsAnimationController;
   Animation _planeSizeAnimation;
   Animation _planeTravelAnimation;
+
+  List<Animation<double>> _dotPositions = [];
 
   double get _planeTopPadding =>
       _minPlanePaddingTop +
       (1 - _planeTravelAnimation.value) * _maxPlaneTopPadding;
 
   double get _maxPlaneTopPadding =>
-      widget.height - _initialPlanePaddingBottom - _planeSize;
+      widget.height -
+      _minPlanePaddingTop -
+      _initialPlanePaddingBottom -
+      _planeSize;
 
   double get _planeSize => _planeSizeAnimation.value;
 
@@ -37,12 +46,16 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
     super.initState();
     _initSizeAnimations();
     _initPlaneTravelAnimations();
+    _initDotAnimationController();
+    _initDotAnimations();
     _planeSizeAnimationController.forward();
   }
 
   @override
   void dispose() {
     _planeSizeAnimationController.dispose();
+    _planeTravelController.dispose();
+    _dotsAnimationController.dispose();
     super.dispose();
   }
 
@@ -52,8 +65,19 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
       width: double.infinity,
       child: Stack(
         alignment: Alignment.center,
-        children: <Widget>[_buildPlane()],
+        children: <Widget>[_buildPlane()]
+          ..addAll(_flightStops.map(_mapFlightStopToDot)),
       ),
+    );
+  }
+
+  Widget _mapFlightStopToDot(stop) {
+    int index = _flightStops.indexOf(stop);
+    bool isStartOrEnd = index == 0 || index == _flightStops.length - 1;
+    Color color = isStartOrEnd ? Colors.red : Colors.green;
+    return AnimatedDot(
+      animation: _dotPositions[index],
+      color: color,
     );
   }
 
@@ -65,7 +89,7 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
           AnimatedPlaneIcon(animation: _planeSizeAnimation),
           Container(
             width: 2.0,
-            height: 240.0,
+            height: _flightStops.length * _cardHeight * 0.8,
             color: Color.fromARGB(255, 200, 200, 200),
           ),
         ],
@@ -87,6 +111,9 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
             widget?.onPlaneFlightStart();
             _planeTravelController.forward();
           });
+          Future.delayed(Duration(milliseconds: 700), () {
+            _dotsAnimationController.forward();
+          });
         }
       });
     _planeSizeAnimation =
@@ -105,5 +132,39 @@ class _PriceTabState extends State<PriceTab> with TickerProviderStateMixin {
       parent: _planeTravelController,
       curve: Curves.fastOutSlowIn,
     );
+  }
+
+  void _initDotAnimations() {
+    //what part of whole animation takes one dot travel
+    final double slideDurationInterval = 0.4;
+    //what are delays between dot animations
+    final double slideDelayInterval = 0.2;
+    //at the bottom of the screen
+    double startingMarginTop = widget.height;
+    //minimal margin from the top (where first dot will be placed)
+    double minMarginTop =
+        _minPlanePaddingTop + _planeSize + 0.5 * (0.8 * _cardHeight);
+
+    for (int i = 0; i < _flightStops.length; i++) {
+      final start = slideDelayInterval * i;
+      final end = start + slideDurationInterval;
+
+      double finalMarginTop = minMarginTop + i * (0.8 * _cardHeight);
+      Animation<double> animation = new Tween(
+        begin: startingMarginTop,
+        end: finalMarginTop,
+      ).animate(
+        new CurvedAnimation(
+          parent: _dotsAnimationController,
+          curve: new Interval(start, end, curve: Curves.easeOut),
+        ),
+      );
+      _dotPositions.add(animation);
+    }
+  }
+
+  void _initDotAnimationController() {
+    _dotsAnimationController = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 500));
   }
 }
